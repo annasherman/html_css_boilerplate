74b9dcc532ce44d6e748dcfc40669742554cf108
Anna Sherman <sherman.anna@gmail.com>
2015-10-29 16:22:40 -0500

    added snazz

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,14 @@
   window.onload = function(){
     //  alert('hey hey hey');
     statusReport('hey hey hey');
+
+    var username = prompt('What is your name?');
+    var mission = prompt('What is your mission?');
+    var contentContainer = document.getElementById('content');
+
+    contentContainer.innerHTML = username + ' is on a mission to ' + mission; 
+     //for gathering the HTML inside of the 'content'. if theres no content, we get an empty string.
+
   }
     function statusReport(message){
       console.log(message);
