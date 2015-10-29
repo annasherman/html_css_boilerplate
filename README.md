f8afde6bbdd7e2ecd9520234ae8a341d91b49828
Anna Sherman <sherman.anna@gmail.com>
2015-10-29 16:06:56 -0500

    added skeleton

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # HTML & CSS Boilerplate
-A great place to get started. This boilerplate uses HTML, CSS, and Javascript. It is a great starting point for **any** project. 
+A great place to get started. This boilerplate uses HTML, CSS, and Javascript. It is a great starting point for **any** project.
---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// this is a comment. learning about load order.
+
+
+  //objects have attributes and abilities !!!!
+  window.onload = function(){
+    //  alert('hey hey hey');
+    statusReport('hey hey hey');
+  }
+    function statusReport(message){
+      console.log(message);
+      return message;
+    }
